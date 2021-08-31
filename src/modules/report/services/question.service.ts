cdac58ca9335477b09b2a1befdfafb94b434ce13
Zhang.P <poterasu.zhang@gmail.com>
2021-08-30 23:23:18 -0400

    question controller done.

--- src/modules/report/services/question.service.ts
+++ src/modules/report/services/question.service.ts
@@ -3,7 +3,7 @@ import { InjectModel } from "@nestjs/mongoose";
 import * as mongoose from "mongoose";
 import { Model } from "mongoose";
 import { IQuestion, IReport, ISection } from "../interfaces/report.interface";
-
+import * as _ from 'lodash';
 const ObjectId = mongoose.Types.ObjectId;
 
 @Injectable()
@@ -16,21 +16,16 @@ export class QuestionService {
   ) {
   }
 
-  async update(report_id: string, section_index: number, question_index: number, updateReportDto: any): Promise<IReport> {
+  async update(report_id: string, section_index: number, question_index: number, updateQuestionDto: any): Promise<IReport> {
     try {
       const report = await this.reportModel.findById(report_id);
-      // report?.sections[section_index]?.questions[question_index]
-    } catch (error) {
-      throw new NotFoundException(`Could nod find report with id ${report_id}`);
-    }
+      const question = report.sections[section_index].questions[question_index];
+      report.sections[section_index].questions[question_index] = _.merge(question, updateQuestionDto)
+      await report.save();
 
-
-    try {
-
-      // await this.reportModel.findByIdAndUpdate(report_id, updateReportDto);
       return await this.reportModel.findById(report_id).exec();
     } catch (error) {
-      throw new Error(error);
+      throw new NotFoundException(`Could nod find`);
     }
   }
 }
